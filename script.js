063629d5ff7509fe8f55036e4815b7dd9ae474bc
Paul Boeck <paul.boeck@diconium.com>
2020-06-17 21:01:10 +0200

    Style charts

--- script.js
+++ script.js
@@ -1,24 +1,101 @@
 const CORONA_TRAFFIC_URL = 'https://knudmoeller.github.io/berlin_corona_cases/data/target/berlin_corona_traffic_light.json';
 
-function render(canvasId, datasets) {
-    var ctx = document.getElementById(canvasId).getContext('2d');
+const COLORS = {
+    green: [0, 128, 0],
+    yellow: [255, 255, 0],
+    red: [255, 0, 0]
+}
+
+function getRGBA(color, alpha = 1) {
+    return `rgba(${color.join(',')},${alpha})`;
+}
+
+const INDICATORS = [
+    {
+        key: 'basic_reproduction_number',
+        canvas: 'chart-reproduction',
+        thresholds: [1.1, 1.2],
+        title: 'Reproduktionsrate R',
+        stepSize: 0.5
+    },
+    {
+        key: 'incidence_new_infections',
+        canvas: 'chart-infections',
+        thresholds: [20, 30],
+        title: 'Neuinfektionen',
+        stepSize: 5
+    },
+    {
+        key: 'icu_occupancy_rate',
+        canvas: 'chart-icu',
+        thresholds: [15, 25],
+        title: 'Krankenhausbetten',
+        stepSize: 5
+    }
+]
+
+function render(key, rawData) {
+    const config = INDICATORS.find(e => e.key === key);
+
+    var ctx = document.getElementById(config.canvas).getContext('2d');
     var chart = new Chart(ctx, {
         type: 'line',
         data: {
-            datasets
+            datasets: [getDataset(rawData, key)]
         },
         options: {
             responsive: true,
             legend: {
                 display: false
             },
+            title: {
+                text: config.title,
+                display: true
+            },
             scales: {
                 xAxes: [{
+                    id: 'x-axis-0',
                     type: 'time',
-                    display: false
+                    display: true,
+                    time: {
+                        stepSize: 5
+                    }
                 }],
                 yAxes: [{
-                    display: false
+                    id: 'y-axis-0',
+                    display: true,
+                    ticks: {
+                        stepSize: config.stepSize,
+                        suggestedMin: 0,
+                        suggestedMax: config.thresholds[1] * 1.15
+                    }
+                }]
+            },
+            annotation: {
+                drawTime: 'beforeDatasetsDraw',
+                annotations: [{
+                    type: 'box',
+                    xScaleID: 'x-axis-0',
+                    yScaleID: 'y-axis-0',
+                    yMin: 0,
+                    yMax: config.thresholds[0],
+                    backgroundColor: getRGBA(COLORS.green, 0.2),
+                    borderWidth: 0,
+                }, {
+                    type: 'box',
+                    xScaleID: 'x-axis-0',
+                    yScaleID: 'y-axis-0',
+                    yMin: config.thresholds[0],
+                    yMax: config.thresholds[1],
+                    backgroundColor: getRGBA(COLORS.yellow, 0.2),
+                    borderWidth: 0,
+                }, {
+                    type: 'box',
+                    xScaleID: 'x-axis-0',
+                    yScaleID: 'y-axis-0',
+                    yMin: config.thresholds[1],
+                    backgroundColor: getRGBA(COLORS.red, 0.2),
+                    borderWidth: 0,
                 }]
             }
         }
@@ -39,9 +116,9 @@ function getDataset(data, key) {
 async function renderDocument() {
     const rawData = await getData();
     rawData.reverse();
-    render('chart-reproduction', [getDataset(rawData, 'basic_reproduction_number')]);
-    render('chart-infections', [getDataset(rawData, 'incidence_new_infections')]);
-    render('chart-icu', [getDataset(rawData, 'icu_occupancy_rate')]);
+    render('basic_reproduction_number', rawData);
+    render('incidence_new_infections', rawData);
+    render('icu_occupancy_rate', rawData);
 }
 
 renderDocument();
